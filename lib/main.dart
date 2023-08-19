633c0483737daea043ce3f95ba4fcb0c1603cd98
devgeniuskb <purveshghevariya77@gmail.com>
2023-08-19 21:12:03 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:health/globals.dart';
+import 'package:health/screens/doctor/main_page_doctor.dart';
+import 'package:health/screens/doctor_or_patient.dart';
+import 'package:health/screens/firebase_auth.dart';
+import 'package:health/screens/my_profile.dart';
+import 'package:health/screens/patient/appointments.dart';
+import 'package:health/screens/patient/doctor_profile.dart';
+import 'package:health/screens/patient/main_page_patient.dart';
+import 'package:health/screens/skip.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase for all platforms(android, ios, web)
+  await Firebase.initializeApp(
+    name: 'health',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? user;
+
+  Future<void> _getUser() async {
+    user = _auth.currentUser;
+  }
+
+  @override
+  void initState() {
+    _getUser();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => user == null ? const Skip() : const DoctorOrPatient(),
+        '/login': (context) => const FireBaseAuth(),
+        '/home': (context) =>
+            isDoctor ? const MainPageDoctor() : const MainPagePatient(),
+        '/profile': (context) => const MyProfile(),
+        '/MyAppointments': (context) => const Appointments(),
+        '/DoctorProfile': (context) => DoctorProfile(),
+      },
+      theme: ThemeData(brightness: Brightness.light),
+      debugShowCheckedModeBanner: false,
+      // home: MainPageDoctor(),
+      // home: ChatRoom(
+      //   userId: '1234',
+      // ),
+    );
+  }
+}
---
+++ lib/screens/patient/home_page.dart
@@ -0,0 +1,154 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:health/firestore_data/top_rated_list.dart';
+import 'package:intl/intl.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  TextEditingController _doctorName = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? user;
+
+  Future<void> _getUser() async {
+    user = _auth.currentUser;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUser();
+    _doctorName = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _doctorName.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String message = "Good";
+    DateTime now = DateTime.now();
+    String currentHour = DateFormat('kk').format(now);
+    int hour = int.parse(currentHour);
+
+    setState(
+      () {
+        if (hour >= 5 && hour < 12) {
+          message = 'Good Morning';
+        } else if (hour >= 12 && hour <= 17) {
+          message = 'Good Afternoon';
+        } else {
+          message = 'Good Evening';
+        }
+      },
+    );
+    return Scaffold(
+      backgroundColor: Colors.white,
+      key: _scaffoldKey,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: <Widget>[Container()],
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Container(
+          padding: const EdgeInsets.only(top: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                //width: MediaQuery.of(context).size.width/1.3,
+                alignment: Alignment.center,
+                child: Text(
+                  message,
+                  style: GoogleFonts.lato(
+                    color: Colors.black54,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+      ),
+      body: SafeArea(
+        child: NotificationListener<OverscrollIndicatorNotification>(
+          onNotification: (OverscrollIndicatorNotification overscroll) {
+            overscroll.disallowIndicator();
+            return true;
+          },
+          child: ListView(
+            physics: const ClampingScrollPhysics(),
+            shrinkWrap: true,
+            children: <Widget>[
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(left: 20, bottom: 10),
+                    child: Text(
+                      "Hello ${user?.displayName}",
+                      style: GoogleFonts.lato(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(left: 20, bottom: 25),
+                    child: Text(
+                      "Let's Find Your\nDoctor",
+                      style: GoogleFonts.lato(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 23, bottom: 10),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "We care for you",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                          color: Colors.blue[800],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 15, right: 15),
+                    child: const TopRatedList(),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
